99cc6628679575273db747466328792d6b7044b9
Enyewashebr <Enyewashebir0@gmail.com>
2025-07-25 17:55:06 +0300

    feat: add Projects component and integrate it into App

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects <span>Completed</span></h1>
+      <p>Crafting spaces, building legacies Explore our portfolio</p>
+    </div>
+  )
+}
+
+export default Projects
