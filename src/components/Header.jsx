774edbb17c06a2483d3b1a966d307ba43d2e5486
Enyewashebr <Enyewashebir0@gmail.com>
2025-07-25 11:23:57 +0300

    refactor: improve Header component structure and formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,21 @@ import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover flex items-center w-full overflow-hidden' style={{backgroundImage:"url('/header_img.png"}} id='Header'>
+    <div
+      className="min-h-screen mb-4 bg-cover flex items-center w-full overflow-hidden"
+      style={{ backgroundImage: "url('/header_img.png" }}
+      id="Header"
+    >
       <Navbar />
+      <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32'>
+        <h2>Explore homes that match your lifestyle</h2>
+        <div>
+          <a href="">Projects</a>
+          <a href="">Contact Us</a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
